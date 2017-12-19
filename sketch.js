ff404ecc3b3a53d27d8574bde018b10905f2e9f9
Alexandru Nastase <nastase.alexandru100@gmail.com>
2017-12-19 11:38:13 +0000

    Added the game

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
-var sqPerLine = 30;
+var sqPerLine = 50;
 var w;
 var grid;
+var newGrid;
 
 
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(900, 900);
     w = floor(width / sqPerLine);
     grid = create2DArray();
 
@@ -30,6 +31,26 @@ function draw() {
             rect(i * w, j * w, w, w);
         }
     }
+
+    newGrid = create2DArray();
+
+    for (var i = 0; i < sqPerLine; i++) {
+        for (var j = 0; j < sqPerLine; j++) {
+
+            var current = grid[i][j];
+            var neighbours = countNearbyLives(i, j);
+
+            if (current == 0 && neighbours == 3) {
+                newGrid[i][j] = 1;
+            } else if (current == 1 && (neighbours < 2 || neighbours > 3)) {
+                newGrid[i][j] = 0;
+            } else {
+                newGrid[i][j] = current;
+            }
+        }
+    }
+
+    grid = newGrid;
 }
 
 function create2DArray() {
@@ -39,4 +60,20 @@ function create2DArray() {
     }
 
     return arr;
+}
+
+function countNearbyLives(i, j) {
+    var lives = 0;
+
+    for (var x = -1; x <= 1; x++) {
+        for (var y = -1; y <= 1; y++) {
+            if (i + x >= 0 && i + x < sqPerLine && j + y >= 0 && j + y < sqPerLine) {
+                if (!(x == 0 && y == 0)) {
+                    lives += grid[i + x][j + y];
+                }
+            }
+        }
+    }
+
+    return lives;
 }
